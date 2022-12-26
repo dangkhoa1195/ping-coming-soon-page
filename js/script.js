8fd1f4bf8e7683afce32fc4558c8cce3fee0d7fd
dangkhoa1195 <ngdangkhoa1195@gmail.com>
2022-12-26 15:25:47 +0700

    add javascript for vaidation

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const btnSubmit = document.querySelector('#btn-submit');
+const inputEmail = document.querySelector('#input-email');
+const textError = document.querySelector('.text-error');
+btnSubmit.addEventListener('click', event => {
+  event.preventDefault();
+  const validRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (!inputEmail.value.match(validRegex)) {
+    textError.style.visibility = 'visible';
+    inputEmail.classList.add('error');
+    inputEmail.placeholder = 'example@email/com';
+  }
+});
+inputEmail.addEventListener('focus', () => {
+  textError.style.visibility = 'hidden';
+  inputEmail.classList.remove('error');
+  inputEmail.placeholder = '';
+});
