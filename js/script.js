1a94d5ca3fdc7c5b6f626acb915b379c5f746595
dangkhoa1195 <ngdangkhoa1195@gmail.com>
2022-12-26 16:03:31 +0700

    update javascript validate for empty value

--- js/script.js
+++ js/script.js
@@ -5,7 +5,11 @@ btnSubmit.addEventListener('click', event => {
   event.preventDefault();
   const validRegex =
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  if (!inputEmail.value.match(validRegex)) {
+  if (inputEmail.value === '') {
+    textError.innerText = 'Whoops! It looks like you forgot to add your email';
+    textError.style.display = 'block';
+  } else if (!inputEmail.value.match(validRegex)) {
+    textError.innerText = 'Please provide a valid email address';
     textError.style.display = 'block';
     inputEmail.classList.add('error');
     inputEmail.placeholder = 'example@email/com';
@@ -16,3 +20,4 @@ inputEmail.addEventListener('focus', () => {
   inputEmail.classList.remove('error');
   inputEmail.placeholder = '';
 });
+const validateEmail = email => {};
